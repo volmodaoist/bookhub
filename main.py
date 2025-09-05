2fa5f050f39e7b9a2f7c5777055c7a53cb6a04bb
volmodaoist <1304548501@qq.com>
2025-09-06 07:57:26 +0800

    rebase: 合并两个commits
    项目初始化
    
    项目初始化x2

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.api.v1.endpoints import book
+
+app = FastAPI()
+app.include_router(book.router, prefix="/api/v1")
+ 
+
+@app.get("/")
+def health():
+    return {"code": 0, "data": "Hello World!"}
+ 
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
