e2366f451d398ce9bd3fbf897c2030f8f845a3ac
volmodaoist <1304548501@qq.com>
2025-10-10 23:20:48 +0800

    feat: 数据访问层添加异常处理 (存在重复代码)

--- app/storage/user/SQLModelUserRepository.py
+++ app/storage/user/SQLModelUserRepository.py
@@ -4,6 +4,21 @@ from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate, UserOut
 from app.storage.user.user_interface import IUserRepository
 
+""" 此处涉及的几个要素:
+ 
+    - 数据事务
+    
+    - 几种异常抛出方式
+        1. raise
+            直接抛出当前异常，保留原始异常栈信息
+        2. raise e       
+            直接抛出当前异常，但是在之前的信息会丢失
+        3. raise Exception("message") from e  
+            附带额外的报错信息 "message"，同时保留原始异常作为上下文, 形成一大串异常链
+        4. raise ValueError("message") 
+            只抛出当前异常栈信息 (在此之前的调用栈信息不被抛出), 终端给出报错信息会相对少一点
+"""
+
 
 # SQLModel ORM 模型与 Pydantic 模型无需经过一层转化，直接使用 SQLModel 模型即可 (以下代码未经过测试, 有待验证)
 class SQLModelUserRepository(IUserRepository):
@@ -31,7 +46,7 @@ class SQLModelUserRepository(IUserRepository):
         self.db.refresh(user)
         return UserOut.model_validate(user)
 
-    def create_batch(self, users: List[UserCreate]) -> List[UserOut]:
+    def create_batch_users(self, users: List[UserCreate]) -> List[UserOut]:
         user_orms = [User(**u.dict()) for u in users]
         self.db.add_all(user_orms)
         self.db.commit()
@@ -46,15 +61,25 @@ class SQLModelUserRepository(IUserRepository):
             return None
         for field, value in user_data.dict(exclude_unset=True).items():
             setattr(user, field, value)
-        self.db.add(user)
-        self.db.commit()
-        self.db.refresh(user)
-        return UserOut.model_validate(user)
+        
+        try:
+            self.db.add(user)
+            self.db.commit()
+            self.db.refresh(user)
+            return UserOut.model_validate(user)
+        except Exception as e:
+            self.db.rollback()
+            raise e
 
     def delete_user(self, student_id: str) -> None:
         statement = select(User).where(User.student_id == student_id)
         user = self.db.exec(statement).first()
         if not user:
             raise ValueError(f"User with student_id {student_id} not found.")
-        self.db.delete(user)
-        self.db.commit()
+        
+        try:
+            self.db.delete(user)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            raise
