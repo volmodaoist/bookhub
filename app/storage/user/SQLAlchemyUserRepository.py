6764baa8c4acdd4be90e3ac633af257405a1d5fd
volmodaoist <1304548501@qq.com>
2025-10-15 22:34:06 +0800

    feat: 封装基于业务主键查询 User(Student)

--- app/storage/user/SQLAlchemyUserRepository.py
+++ app/storage/user/SQLAlchemyUserRepository.py
@@ -1,47 +1,14 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import select, update, delete
 
+from app.core.db import transaction
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate, UserOut, BatchUsersOut
 from app.storage.user.user_interface import IUserRepository
 
-from contextlib import contextmanager
+
 from typing import Optional, List, Dict, Union
 
-import logging
 
-
-""" 此处涉及的几个要素:
- 
-    - 数据事务
-
-    
-    - 几种异常抛出方式
-        1. raise
-            直接抛出当前异常，保留原始异常栈信息
-        2. raise e       
-            直接抛出当前异常，但是在之前的信息会丢失
-        3. raise Exception("message") from e  
-            附带额外的报错信息 "message"，同时保留原始异常作为上下文, 形成一大串异常链
-        4. raise ValueError("message") 
-            只抛出当前异常栈信息 (在此之前的调用栈信息不被抛出), 终端给出报错信息会相对少一点
-            
-    数据访问层只抛异常，不对异常进行过多的处理，异常的处理交给上层来解决(通常是接口层, 或者业务逻辑层, 取决于设计者)
-"""
-
-
-# 使用 Python上下文管理器来处理数据库事务, 在失败的时候自动回滚, 通过事务管理器来保证原子性
-# 成功时提交事务(commit),  commit 完成之后才能调用 refresh 获取最新状态;  失败时回滚事务, 打印异常调用栈，并且重新向上抛出异常!
-@contextmanager
-def transaction(db: Session):
-    try:
-        yield db
-        db.commit()
-    except Exception as e:
-        logging.exception(e)
-        db.rollback()
-        raise
-    
     
 
 # 这是 PySQL+SQLAlchemy 实现的用户仓库(业务逻辑传入的参数是一个 IUserRepository 类型，而不是具体的子类)
@@ -50,13 +17,18 @@ class SQLAlchemyUserRepository(IUserRepository):
     def __init__(self, db: Session):
         self.db = db
 
+    def query_student(self, student_id: str) -> User:
+        user = self.db.query(User).filter(User.student_id == student_id).first()
+        return user        
+
     def get_user_by_uid(self, uid: int) -> Optional[UserOut]:
         user = self.db.query(User).filter(User.uid == uid).first()
         return UserOut.model_validate(user) if user else None
 
     def get_user_by_student_id(self, student_id: str) -> Optional[UserOut]:
-        user = self.db.query(User).filter(User.student_id == student_id).first()
+        user = self.query_student(student_id)
         return UserOut.model_validate(user) if user else None
+
 
     def get_batch_users(self, page: int, page_size: int) -> Optional[BatchUsersOut]:
         # 获取用户列表
@@ -101,7 +73,7 @@ class SQLAlchemyUserRepository(IUserRepository):
 
 
     def update_user(self, student_id: str, user_data: UserUpdate) -> Optional[UserOut]:
-        user = self.db.query(User).filter(User.student_id == student_id).first()
+        user = self.query_student(student_id)
         
         if not user:
             return None
@@ -117,7 +89,7 @@ class SQLAlchemyUserRepository(IUserRepository):
 
 
     def delete_user(self, student_id: str) -> Optional[UserOut]:
-        user = self.db.query(User).filter(User.student_id == student_id).first()
+        user = self.query_student(student_id)
         
         if not user:
             raise ValueError(f"User with student_id {student_id} not found.")
